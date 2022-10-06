a8f12a6e7fff29164c088a5a747f095485d3f5d1
Jerome Wong <jeromewongjh@gmail.com>
2022-10-06 22:07:40 +0800

    created page content placeholder

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,32 @@
 <template>
   <main>
     <Sidebar />
+    <PageContentPlaceholder>
+      <component :is="childComponent"></component>
+    </PageContentPlaceholder>
   </main>
 </template>
 
 <script>
 import Sidebar from './components/Sidebar.vue';
+import Button from './components/Button.vue';
+import PageContentPlaceholder from './components/PageContentPlaceholder.vue'
+import SampleContent from './components/SampleContent.vue';
 export default {
   name: ' App',
   components: {
-    Sidebar
+    Button,
+    Sidebar,
+    PageContentPlaceholder,
+    SampleContent
+  },
+  data() {
+    return {
+      childComponent: 'SampleContent',
+    }
   }
 }
 </script>
-
 <style scoped>
 
 </style>
---
+++ src/components/PageContentPlaceholder.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col dark:bg-gray-900 h-screen">
+        <section className="bg-white dark:bg-gray-900 pb-20 ">
+            <div className="items-center ml-20 mr-6 my-7 mx-auto max-w-screen-2xl ">
+                <slot>Content</slot>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+</script>
