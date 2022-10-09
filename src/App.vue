7bbb688ed7f06e18eca2334b37faf24ced75a59d
Nicklaus Chiok <tschiok.2021@scis.smu.edu.sg>
2022-10-09 19:26:21 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 
 <template>
   <main>
+    <Navbar />
     <Sidebar />
   </main>
 </template>
 
 <script>
 import Sidebar from './components/Sidebar.vue';
+import Navbar from './components/Navbar.vue';
 export default {
   name: ' App',
   components: {
-    Sidebar
+    Sidebar, 
+    Navbar
   }
 }
 </script>
