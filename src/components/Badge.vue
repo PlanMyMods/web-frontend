15fcb7880862bd7253af80abeff896a462d6cb96
Jerome Wong <jeromewongjh@gmail.com>
2022-10-09 13:24:34 +0800

    changed badge image prop name

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -2,7 +2,7 @@
 
     <span
         class="bg-blue-100 inline-flex text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 gap-1 items-center justify-center rounded dark:bg-indigo-200 dark:text-indigo-900">
-        <img class="w-3 h-3" :src="getImageUrl(badgeImage)">
+        <img class="w-3 h-3" :src="getImageUrl(badgeImagePath)">
         <p>
             {{badgeName}}
         </p>
@@ -12,8 +12,7 @@
 <script>
 export default {
     props: {
-
-        'badgeImage': String,
+        'badgeImagePath': String,
         'badgeName': String
     },
     setup() {
