3660b96313d3150f867f0dfbfd0fe80b8be9237a
Nicklaus Chiok <tschiok.2021@scis.smu.edu.sg>
2022-10-12 20:39:46 +0800

    added placeholder subject fields in the dropdown menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,11 +20,28 @@
                                 clip-rule="evenodd"></path>
                         </svg></button>
 
+                        <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(897px, 5637px, 0px);">
+                            <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                                <li>
+                                    <button type="button" class="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">IS - Information Systems</button>
+                                </li>
+                                <li>
+                                    <button type="button" class="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">DSA - Data Science Analytics</button>
+                                </li>
+                                <li>
+                                    <button type="button" class="inline-flex py-2 px-4 w-full text-center hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">SE - Software Engineering</button>
+                                </li>
+                                <li>
+                                    <button type="button" class="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">POSC - Political Science</button>
+                                </li>
+                            </ul>
+                        </div>
+
                     <!-- searchbar -->
                     <div class="flex w-full">
                         <input type="search" id="search-dropdown"
                             class="scale-0 md:scale-100 p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                            placeholder="Search Modules" required>
+                            placeholder="Search ... " required>
                         <button type="submit"
                             class=" p-2.5 text-sm font-medium text-white bg-blue-700 rounded-lg md:rounded-r-lg md:rounded-l-none border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor"
