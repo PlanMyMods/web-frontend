ae504013be4da34e57f8d7c05b3acb91ab9b0185
Jerome Andre Wong <68373112+jeromeandrewong@users.noreply.github.com>
2022-10-19 17:52:57 +0800

    created new sidebar that displaces page content to right when active,… (#24)
    created new sidebar that displaces page content to right when active, removed search bar to add as separate component outside of nav

--- src/components/ModuleCard.vue
+++ src/components/ModuleCard.vue
@@ -1,11 +1,8 @@
 <template>
   <div
-    class="dark:bg-gray-800 px-6 py-6 md:px-8 md:py-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col lg:flex-row justify-between space-y-10 lg:space-x-10 max-w-screen-2xl"
-  >
+    class="dark:bg-gray-800 px-6 py-6 md:px-8 md:py-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col lg:flex-row justify-between space-y-10 lg:space-x-10 max-w-screen-2xl">
     <section>
-      <h1
-        class="text-xl lg:text-3xl dark:text-blue-300 text-blue-600 font-extrabold"
-      >
+      <h1 class="text-xl lg:text-3xl dark:text-blue-300 text-blue-600 font-extrabold">
         {{ title }}
       </h1>
       <div class="flex space-x-2 mt-3">
@@ -13,9 +10,7 @@
           <Badge :badge-name="badge.name" :badge-image-path="badge.imagePath" />
         </div>
       </div>
-      <p
-        class="text-sm lg:text-lg dark:text-gray-300 text-gray-500 mt-3 pt-2 pb-2"
-      >
+      <p class="text-sm lg:text-lg dark:text-gray-300 text-gray-500 mt-3 pt-2 pb-2">
         {{ shortDescription }}
       </p>
       <div class="text-xs lg:text-lg text-black dark:text-gray-300">
@@ -30,55 +25,39 @@
 
     <section class="text-xs lg:text-base">
       <div class="flex text-black dark:text-gray-300">
-        <button
-          v-for="term in valid_terms"
-          :key="term"
-          @click="updateSelectedTerm(term)"
-          class="px-3 pb-2 border-b w-[5rem] lg:w-[6rem] font-medium text-[#6B7280;]"
-          :class="{
+        <button v-for="term in valid_terms" :key="term" @click="updateSelectedTerm(term)"
+          class="px-3 pb-2 border-b w-[5rem] lg:w-[6rem] font-medium text-[#6B7280;]" :class="{
             'border-b-2 dark:border-b-blue-300 border-b-blue-600 dark:text-blue-300 text-blue-600':
               isTermSelected(term),
-          }"
-          :disabled="isTermValid(term)"
-        >
+          }" :disabled="isTermValid(term)">
           Term {{ term }}
         </button>
       </div>
-      <div class="mt-5 space-y-3 px-6 dark:text-gray-300">
+      <div class="mt-5 space-y-3  dark:text-gray-300">
         <div>
           <div class="font-bold mb-2">Exams:</div>
           <div>
             {{ format(terms[selectedIndex].exam.start, "dd-MMMM-yyyy") }} •
             {{
-              getTimeDifference(
-                terms[selectedIndex].exam.start,
-                terms[selectedIndex].exam.end
-              )
+            getTimeDifference(
+            terms[selectedIndex].exam.start,
+            terms[selectedIndex].exam.end
+            )
             }}
             hrs
           </div>
         </div>
         <div>
           <div class="font-bold mb-2">Course Assessment:</div>
-          <div
-            v-for="(assessment, index) in terms[selectedIndex].assessment"
-            :key="index"
-            class="flex items-center space-x-2"
-          >
+          <div v-for="(assessment, index) in terms[selectedIndex].assessment" :key="index"
+            class="flex items-center space-x-2">
             <div class="w-32">{{ assessment.name }}</div>
             <div class="w-8">{{ assessment.weightage * 100 }}%</div>
             <div class="hidden sm:flex space-x-0.5">
-              <div
-                v-for="(_, index) in Array(
-                  Math.floor(assessment.weightage * 10)
-                )"
-                :key="index"
-                class="h-2.5 w-4 rounded-sm bg-blue-100"
-              ></div>
-              <div
-                v-if="(assessment.weightage * 100) % 10 !== 0"
-                class="h-2.5 w-2 rounded-l-sm bg-blue-100"
-              ></div>
+              <div v-for="(_, index) in Array(
+                Math.floor(assessment.weightage * 10)
+              )" :key="index" class="h-2.5 w-4 rounded-sm bg-blue-100"></div>
+              <div v-if="(assessment.weightage * 100) % 10 !== 0" class="h-2.5 w-2 rounded-l-sm bg-blue-100"></div>
             </div>
           </div>
         </div>
@@ -90,9 +69,7 @@
         </div>
 
         <div>
-          <a class="dark:text-blue-300 text-blue-600" :href="courseLink"
-            >Course Outline</a
-          >
+          <a class="dark:text-blue-300 text-blue-600" :href="courseLink">Course Outline</a>
         </div>
       </div>
     </section>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,17 +1,13 @@
+
 <template>
-    <div class="bg-gray-100 dark:bg-gray-800 p-5 pt-8 relative h-screen min-w-[14rem] duration-300 w-56"
+    <div class="bg-gray-50 dark:bg-gray-800 p-5 relative h-screen min-w-[14rem] duration-300 w-56 hidden md:block"
         :class="{'w-20 min-w-[5rem]':showSideBar}">
-        <Button @click="showSideBar = !showSideBar" class=" text-gray-500  absolute right-5 top-9 dark:text-gray-400"
+        <Button @click="showSideBar = !showSideBar"
+            class=" text-gray-500  absolute rounded-full border border-solid border-gray-200 dark:border-gray-500 dark:bg-gray-800 bg-gray-50 -right-4 top-14 dark:text-gray-400 "
             :class="{'rotate-180':showSideBar}">
-            <img class="w-6 h-6" src="/assets/arrowLeft.svg" alt="">
+            <img class="w-4 h-4" src="/assets/arrowLeft.svg" alt="">
         </Button>
-        <div class="inline-flex">
-            <h1 class="block float-left font-bold tracking-widest p-[10px] dark:text-white"
-                :class="{'scale-0':showSideBar}">
-                <!-- BeforeClass -->
-            </h1>
-        </div>
-        <ul class="space-y-2 p-[10px]  mt-[15px]" :class="{'px-0':showSideBar}">
+        <ul class="space-y-2 p-[10px]" :class="{'px-0':showSideBar}">
             <li>
                 <div class="p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700">
                     <a href='/'
