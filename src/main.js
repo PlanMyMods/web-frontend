e2011b8ed44eedeaad348336d70e0425f5fb5667
Jerome Wong <jeromewongjh@gmail.com>
2022-10-20 18:38:51 +0800

    removed unnecessary imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import vueClickOutsideElement from "vue-click-outside-element";
 
 const app = createApp(App);
 
-app.use(vueClickOutsideElement);
 app.mount("#app");
