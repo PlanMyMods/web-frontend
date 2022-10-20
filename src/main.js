46245d0f7fbe9fdd21e010f4f936cde7d835c861
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-10-20 10:19:11 +0800

    VueRouter
    Added VueRouter for us to navigate through our diff pages. However, I think still need to link it to the current Sidebar?

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import VueRouter from 'vue-router'
+import Home from './views/Home.vue'
+
+
+Vue.use(VueRouter)
+
+// will add more routes as we create more pages
+const routes = [
+    {path: '/', component: Home}, 
+    {path: '/modules', component: Modules}, 
+]
+
+const router = new VueRouter({
+    routes, 
+    mode: 'history'
+})
 
 createApp(App).mount("#app");
