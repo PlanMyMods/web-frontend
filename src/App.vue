3e822a4dbc70a6d079cd9caa3ba9dc3b0da98b4e
ong-zijian <zijian.ong.2021@scis.smu.edu.sg>
2022-10-21 13:17:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -170,6 +170,9 @@
         </div>
       </div>
     </div>
+    <!-- This is for testing only! -->
+    <button @click="getCoursePrerequisite(course)">Click here</button>
+    <button @click="returndb()">Here!</button>
   </main>
 </template>
 
@@ -181,7 +184,7 @@ import PageContentPlaceholder from "./components/PageContentPlaceholder.vue";
 import SampleContent from "./components/SampleContent.vue";
 import ModuleCard from "./components/ModuleCard.vue";
 import db from './firebase.js';
-import { doc, getDoc } from "firebase/firestore";
+import {collection, query, where, doc, getDoc } from "firebase/firestore";
 
 export default {
   name: " App",
@@ -196,19 +199,99 @@ export default {
   data() {
     return {
       childComponent: 'SampleContent',
+      course: "IS216"
     }
   },
   methods: {
-    async returndb(){
-      const docRef = doc(db, "Modules", "CS101");
+    //Sample code: return everything in each course code
+    async returndbSample(){
+      const docRef = doc(db, "Modules", "IS101");
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         console.log("Document data:", docSnap.data());
+        return docSnap.data();
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
       }
-    }
+    },
+
+    //-----------------------------------------Module Collection-----------------------------------------------
+    //call the full details of each course by course code;
+    //returns the object of the whole course
+    async getFullCourse(code){
+      const docRef = doc(db, "Modules", code);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        return docSnap.data();
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+    
+     //call the course name of each course by course code;
+    //returns a string of the course name
+    async getCourseName(code){
+      const docRef = doc(db, "Modules", code);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().name);
+        return docSnap.data().name;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+
+    //call the course description of each course by course code;
+    //returns an object of the long & short description
+    async getCourseDescription(code){
+      const docRef = doc(db, "Modules", code);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().description);
+        return docSnap.data().description;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+
+    //call the course link of each course by course code;
+    //returns a string of the link
+    async getCourseLink(code){
+      const docRef = doc(db, "Modules", code);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().link);
+        return docSnap.data().link;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+    
+    //call the course prerequisite of each course by course code;
+    //returns a string of the link
+    async getCoursePrerequisite(code){
+      const docRef = doc(db, "Modules", code);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().prerequisites);
+        return docSnap.data().prerequisites;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+
+    //retrieve keys: find out how to query them
+
+    //---------------------------------------Professor Collection---------------------------------------------
+    
+
 
   } 
 }
