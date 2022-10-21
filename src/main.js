fa001facd3d4157dbf1fc321a67f03b29fe69841
GabeW8 <111336507+GabeW8@users.noreply.github.com>
2022-10-22 01:45:09 +0800

    add vue router (#35)
    * VueRouter
    
    Added VueRouter for us to navigate through our diff pages. However, I think still need to link it to the current Sidebar?
    
    * Update main.js
    
    * Minor update
    
    Not many changes - just tried to debug why Vue Router wasn't working for me
    
    * Update index.html
    
    * Update main.js
    
    * Update main.js
    
    * HTML additions to Router
    
    Small changes to Router
    
    * test
    
    test
    
    * Update main.js
    
    * Index.html - router implementation
    
    Changed the <a href> tags to <router-link>
    
    * Update Sidebar.vue
    
    * Update main.js
    
    * update dependency lock
    
    Co-authored-by: David Lin <13061926+DavidLHW@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import Home from './views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {path: '/', component: Home}, 
+    {path: '/modules', component: Modules}, 
+    
+]
+
+const router = new VueRouter({
+    routes
+})
+
+App.use(router);
 
 createApp(App).mount("#app");
