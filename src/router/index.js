dc9aedba66400ee80fcdeb339afe0f483f8b9135
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-10-24 13:41:36 +0800

    fix: 404 on reload (#42)
    * feat: setup router
    
    * extract router logic
    
    * refactor: code styling
    
    * refactor: code styling and decoupling
    
    * build: add dev dependency for esbuild
    
    * fix: 404 on reload

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Settings from "@/views/SettingsView.vue";
 import ModuleListView from "@/views/ModuleListView.vue";
 
 const routes = [
+  { path: "/", redirect: { name: "Timetable" } },
   { path: "/timetable", name: "Timetable", component: Timetable },
   { path: "/modules", name: "Modules", component: ModuleListView },
   { path: "/settings", name: "Settings", component: Settings },
