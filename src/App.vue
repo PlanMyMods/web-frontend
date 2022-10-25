62ad375e3c35d2a6eb28e6b031cbed93e3000ca5
Jerome Andre Wong <68373112+jeromeandrewong@users.noreply.github.com>
2022-10-25 12:34:47 +0800

    Searchbar (#43)
    * refactored layout for card component
    
    * feat:added searchbar for navbar
    
    * feat: added searchbar for sidebar
    
    * refactored code for build
    
    * refactored code for build
    
    * Update App.vue
    
    * Update Searchbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <main class="w-screen flex flex-col fixed">
     <Navbar class="" />
-    <div class="flex h-[calc(100vh-4rem)]">
+    <!-- <div class="flex h-[calc(100vh-4rem)]"> -->
+    <div class="flex h-screen">
       <Sidebar />
       <div class="flex-auto dark:bg-gray-900 bg-grey-2 overflow-y-scroll">
         <div class="items-center mx-auto max-w-screen-2xl p-10 space-y-5">
@@ -24,4 +25,6 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <div class="p-4 md:px-0 bg-gray-50 dark:bg-gray-800">
+        <div class="flex items-center text-base font-normal text-gray-900 rounded-lg dark:text-white ">
+            <input type='search' placeholder="Search" class="w-full bg-transparent rounded-lg focus:outline-none" />
+        </div>
+    </div>
+</template>
