1a6f57d6a29a403d3439b4db652654b40c8b1d04
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-10-31 03:32:54 +0800

    update moduleCard

--- src/components/ModuleCard.vue
+++ src/components/ModuleCard.vue
@@ -1,35 +1,50 @@
 <template>
   <div
-    class="dark:bg-gray-800 px-6 py-6 md:px-8 md:py-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col xl:flex-row justify-between space-y-10 lg:space-x-10 max-w-screen-2xl bg-gray-50 lg:grid lg:grid-cols-4 xl:grid-cols-5">
+    class="dark:bg-gray-800 px-6 py-6 md:px-8 md:py-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col xl:flex-row justify-between space-y-10 lg:space-x-10 max-w-screen-2xl bg-gray-50 lg:grid lg:grid-cols-4 xl:grid-cols-5"
+  >
     <section class="lg:col-span-2 xl:col-span-3">
-      <h1 class="text-xl lg:text-3xl dark:text-blue-300 text-blue-600 font-extrabold">
+      <div
+        class="text-lg md:text-lg lg:text-2xl dark:text-blue-300 text-blue-600 font-extrabold"
+      >
         {{ title }}
-      </h1>
-      <div class="flex space-x-2 mt-3">
-        <div v-for="(badge, index) in badges" :key="index" class="">
-          <Badge :badge-name="badge.name" :badge-image-path="badge.imagePath" />
-        </div>
       </div>
-      <p class="text-sm lg:text-lg dark:text-gray-300 text-gray-500 mt-3 pt-2 pb-2">
+      <div class="flex space-x-2 mt-3">
+        <Badge
+          v-for="(badge, index) in badges"
+          :key="index"
+          :badge-name="badge.name"
+          :badge-image-path="badge.imagePath"
+        />
+      </div>
+      <div
+        class="text-xs lg:text-base dark:text-gray-300 text-gray-500 mt-3 pt-2 pb-2"
+      >
         {{ shortDescription }}
-      </p>
-      <div class="text-xs lg:text-lg text-black dark:text-gray-300">
-        <p class="font-bold mt-3">Preclusions:</p>
-        <p class="mt-2">{{ preclusion }}</p>
-        <p class="font-bold mt-3">Prerequisite:</p>
-        <p class="mt-2">{{ prerequisite }}</p>
-        <p class="font-bold mt-3">Co-requisite:</p>
-        <p class="mt-2">{{ corequisite }}</p>
+      </div>
+      <div class="text-xs lg:text-base text-black dark:text-gray-300">
+        <div class="font-bold mt-3">Preclusions:</div>
+        {{ preclusion }}
+        <div class="font-bold mt-3">Prerequisite:</div>
+        {{ prerequisite }}
+        <div class="font-bold mt-3">Co-requisite:</div>
+        {{ corequisite }}
       </div>
     </section>
 
-    <section class="text-xs lg:text-base lg:col-span-2">
+    <section class="text-xs lg:text-sm lg:col-span-2">
       <div class="flex text-black dark:text-gray-300">
-        <button v-for="term in valid_terms" :key="term" @click="updateSelectedTerm(term)"
-          class="px-3 pb-2 border-b w-[5rem] lg:w-[6rem] font-medium text-[#6B7280;]" :class="{
+        <button
+          v-for="term in validTerms"
+          :key="term"
+          @click="updateSelectedTerm(term)"
+          class="px-3 pb-2 border-b w-[5rem] font-medium text-[#6B7280;]"
+          :class="{
             'border-b-2 dark:border-b-blue-300 border-b-blue-600 dark:text-blue-300 text-blue-600':
               isTermSelected(term),
-          }" :disabled="isTermValid(term)">
+            'dark:text-gray-500 text-gray-700': isTermValid(term),
+          }"
+          :disabled="isTermValid(term)"
+        >
           Term {{ term }}
         </button>
       </div>
@@ -39,25 +54,35 @@
           <div>
             {{ format(terms[selectedIndex].exam.start, "dd-MMMM-yyyy") }} •
             {{
-                getTimeDifference(
-                  terms[selectedIndex].exam.start,
-                  terms[selectedIndex].exam.end
-                )
+              getTimeDifference(
+                terms[selectedIndex].exam.start,
+                terms[selectedIndex].exam.end
+              )
             }}
             hrs
           </div>
         </div>
         <div>
           <div class="font-bold mb-2">Course Assessment:</div>
-          <div v-for="(assessment, index) in terms[selectedIndex].assessment" :key="index"
-            class="flex items-center space-x-2">
+          <div
+            v-for="(assessment, index) in terms[selectedIndex].assessment"
+            :key="index"
+            class="flex items-center space-x-2"
+          >
             <div class="w-32">{{ assessment.name }}</div>
             <div class="w-8">{{ assessment.weightage * 100 }}%</div>
-            <div class="hidden sm:flex space-x-0.5">
-              <div v-for="(_, index) in Array(
-                Math.floor(assessment.weightage * 10)
-              )" :key="index" class="h-2.5 w-4 rounded-sm bg-blue-100"></div>
-              <div v-if="(assessment.weightage * 100) % 10 !== 0" class="h-2.5 w-2 rounded-l-sm bg-blue-100"></div>
+            <div class="hidden xs:flex space-x-0.5">
+              <div
+                v-for="(_, index) in Array(
+                  Math.floor(assessment.weightage * 10)
+                )"
+                :key="index"
+                class="h-2.5 w-4 rounded-sm bg-blue-100"
+              ></div>
+              <div
+                v-if="(assessment.weightage * 100) % 10 !== 0"
+                class="h-2.5 w-2 rounded-l-sm bg-blue-100"
+              ></div>
             </div>
           </div>
         </div>
@@ -69,7 +94,12 @@
         </div>
 
         <div>
-          <a class="dark:text-blue-300 text-blue-600" :href="courseLink">Course Outline</a>
+          <a
+            class="dark:text-blue-300 text-blue-600"
+            :href="courseLink"
+            target="_blank"
+            >Course Outline</a
+          >
         </div>
       </div>
     </section>
@@ -82,7 +112,7 @@ import { ref } from "vue";
 import { format } from "date-fns";
 
 export default {
-  setup(props) {
+  setup() {
     let selectedIndex = ref(0);
     return { selectedIndex };
   },
@@ -132,7 +162,7 @@ export default {
         );
       },
     },
-    valid_terms: {
+    validTerms: {
       validator(value) {
         return (
           Array.isArray(value) &&
--- src/views/ModuleListView.vue
+++ src/views/ModuleListView.vue
@@ -1,11 +1,14 @@
 <template>
   <ModuleCard
-    title="IS446 Managing Customer Relations with Analytics: Asian Insights"
-    short-description="some short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short text"
-    preclusion="CS101"
-    prerequisite="CS101"
-    corequisite="CS101"
-    :valid_terms="['1', '2', '3A', '3B']"
+    v-for="mod in moduleList"
+    :key="mod.code"
+    :title="mod.name"
+    :shortDescription="mod.description.long"
+    :validTerms="validTerms"
+    :prerequisite="mod.prerequisites.string ? mod.prerequisites.string : 'None'"
+    :preclusion="mod.prerequisites.string ? mod.prerequisites.string : 'None'"
+    :corequisite="mod.prerequisites.string ? mod.prerequisites.string : 'None'"
+    :courseLink="mod.link"
     :terms="[
       {
         term: 'AY2223/2',
@@ -46,130 +49,28 @@
           year: 1,
         },
       },
-    ]"
-    course-link="https://smu.sg/cdd-is112"
-    :badges="[
-      { imagePath: '/assets/projects.svg', name: 'Year 1' },
-      { imagePath: '/assets/projects.svg', name: 'Projects' },
-    ]"
-  />
-  <ModuleCard
-    title="IS446 Managing Customer Relations with Analytics: Asian Insights"
-    short-description="some short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short text"
-    preclusion="CS101"
-    prerequisite="CS101"
-    corequisite="CS101"
-    :valid_terms="['1', '2', '3A', '3B']"
-    :terms="[
-      {
-        term: 'AY2223/2',
-        assessment: [
-          {
-            name: 'Class Participation',
-            weightage: 0.1,
-          },
-          {
-            name: 'Lab Test 1',
-            weightage: 0.1,
-          },
-          {
-            name: 'Lab Test 2',
-            weightage: 0.25,
-          },
-          {
-            name: 'Group Project',
-            weightage: 0.25,
-          },
-          {
-            name: 'Final Exam',
-            weightage: 0.3,
-          },
-        ],
-        exam: {
-          start: 1669813200,
-          end: 1669820400,
-          day: 3,
-        },
-        recommended: {
-          hours: {
-            homework: 0.3,
-            lesson: 0.3,
-            project: 0.4,
-            total_weekly: 8,
-          },
-          year: 1,
-        },
-      },
-    ]"
-    course-link="https://smu.sg/cdd-is112"
-    :badges="[
-      { imagePath: '/assets/projects.svg', name: 'Year 1' },
-      { imagePath: '/assets/projects.svg', name: 'Projects' },
-    ]"
-  />
-  <ModuleCard
-    title="IS446 Managing Customer Relations with Analytics: Asian Insights"
-    short-description="some short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short textsome short text"
-    preclusion="CS101"
-    prerequisite="CS101"
-    corequisite="CS101"
-    :valid_terms="['1', '2', '3A', '3B']"
-    :terms="[
-      {
-        term: 'AY2223/2',
-        assessment: [
-          {
-            name: 'Class Participation',
-            weightage: 0.1,
-          },
-          {
-            name: 'Lab Test 1',
-            weightage: 0.1,
-          },
-          {
-            name: 'Lab Test 2',
-            weightage: 0.25,
-          },
-          {
-            name: 'Group Project',
-            weightage: 0.25,
-          },
-          {
-            name: 'Final Exam',
-            weightage: 0.3,
-          },
-        ],
-        exam: {
-          start: 1669813200,
-          end: 1669820400,
-          day: 3,
-        },
-        recommended: {
-          hours: {
-            homework: 0.3,
-            lesson: 0.3,
-            project: 0.4,
-            total_weekly: 8,
-          },
-          year: 1,
-        },
-      },
-    ]"
-    course-link="https://smu.sg/cdd-is112"
-    :badges="[
-      { imagePath: '/assets/projects.svg', name: 'Year 1' },
-      { imagePath: '/assets/projects.svg', name: 'Projects' },
     ]"
   />
 </template>
 
 <script>
 import ModuleCard from "@/components/ModuleCard.vue";
+import { returnModuleObject } from "@/utils/firebasemethod";
+
 export default {
   name: "ModuleListView",
   components: { ModuleCard },
+  async mounted() {
+    this.moduleList = await returnModuleObject();
+  },
   data() {
-    return {};
+    return {
+      validTerms: ["1", "2", "3A", "3B"],
+      moduleList: [],
+    };
+  },
+  methods: {
+    returnModuleObject,
   },
 };
 </script>
