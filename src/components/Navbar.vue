4a5994eaf2f48b177653263363d5c4782c4c40a4
Jerome Andre Wong <68373112+jeromeandrewong@users.noreply.github.com>
2022-11-01 20:03:15 +0800

    refactor: searchbar component and nav menu alignments (#50)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,7 @@
         </div>
       </nav>
       <!-- nav menu -->
-      <div class="py-2.5 px-5 w-full md:hidden md:w-auto bg-gray-50 dark:bg-gray-800"
+      <div class="py-2.5 px-8 w-full md:hidden md:w-auto bg-gray-50 dark:bg-gray-800"
         :class="{ hidden: showNavMenu === false }">
         <ul
           class="flex flex-col p-4 bg-gray-50 rounded-lg border border-gray-200 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
