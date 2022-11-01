0f867a4aa0f6914c9429db5071c05a8458916caa
Jerome Wong <jeromewongjh@gmail.com>
2022-11-01 18:18:15 +0800

    minor refactor for searchbar component and nav menu alignments

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,8 +1,17 @@
 <template>
 
-    <div class="p-4 md:px-0 bg-gray-50 dark:bg-gray-800">
-        <div class="flex items-center text-base font-normal text-gray-900 rounded-lg dark:text-white ">
-            <input type='search' placeholder="Search" class="w-full bg-transparent rounded-lg focus:outline-none" />
+
+
+
+    <div class="px-8 pb-3 pt-1 md:px-0 bg-gray-50 dark:bg-gray-800">
+
+        <div class="relative mx-auto">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <img class="w-5 h-5" src="/assets/search.svg" alt="" />
+            </div>
+            <input type="text" id="email-address-icon"
+                class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Find Modules">
         </div>
     </div>
 </template>
