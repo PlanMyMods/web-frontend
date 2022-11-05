d64b56a9ae991cb3097713b923d830277cbaad60
Nicklaus Chiok <tschiok.2021@scis.smu.edu.sg>
2022-11-05 11:44:17 +0800

    help

---
+++ src/components/TimetableRow.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="grid grid-cols-4">
+      <div>
+        <div class="grid grid-cols-4">
+
+          <div class="border-2 border-gray-600 text-xs p-auto">Mon</div>
+          <div class="px-4 py-2 border-2 border-gray-600">02</div>
+          <div class="px-4 py-2 border-2 border-gray-600">03</div>
+          <div class="px-4 py-2 border-2 border-gray-600">04</div>
+
+        </div>
+      </div>
+
+      <div>
+        <div class="grid grid-cols-4">
+
+          <div class="px-4 py-2 border-2 border-gray-600">01</div>
+          <div class="px-4 py-2 border-2 border-gray-600">02</div>
+          <div class="px-4 py-2 border-2 border-gray-600">03</div>
+          <div class="px-4 py-2 border-2 border-gray-600">04</div>
+
+        </div>
+      </div>
+
+      <div>
+        <div class="grid grid-cols-4">
+
+          <div class="px-4 py-2 border-2 border-gray-600">01</div>
+          <div class="px-4 py-2 border-2 border-gray-600">02</div>
+          <div class="px-4 py-2 border-2 border-gray-600">03</div>
+          <div class="px-4 py-2 border-2 border-gray-600">04</div>
+
+        </div>
+      </div>
+
+      <div>
+        <div class="grid grid-cols-4">
+
+          <div class="px-4 py-2 border-2 border-gray-600">01</div>
+          <div class="px-4 py-2 border-2 border-gray-600">02</div>
+          <div class="px-4 py-2 border-2 border-gray-600">03</div>
+          <div class="px-4 py-2 border-2 border-gray-600">04</div>
+
+        </div>
+      </div>
+
+
+    </div>
+</template>
--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -1,3 +1,18 @@
 <template>
   <h1 class="dark:text-white">Timetable</h1>
+
+  <div class="container">
+    <TimetableRow></TimetableRow>
+    <TimetableRow></TimetableRow>
+    
+  </div>
 </template>
+
+<script>
+  import TimetableRow from "@/components/TimetableRow.vue"
+
+  export default {
+    name: "TimetableView",
+    components: { TimetableRow }
+  }
+</script>
