7726ed2bf1a35c38d94d75ce2834f1bbeff72cbe
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-11-08 01:42:03 +0800

    created Register view page and added to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import Timetable from "@/views/TimetableView.vue";
 import Settings from "@/views/SettingsView.vue";
 import ModuleListView from "@/views/ModuleListView.vue";
+import Register from "@/views/Register.vue";
 
 const routes = [
   { path: "/", redirect: { name: "Timetable" } },
   { path: "/timetable", name: "Timetable", component: Timetable },
   { path: "/modules", name: "Modules", component: ModuleListView },
   { path: "/settings", name: "Settings", component: Settings },
+  { path: "/register", name: "Register", component: Register },
 ];
 
 const router = createRouter({
