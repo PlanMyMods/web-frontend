e2c70cb88600072704134bd67a4f2bcffca4cdbd
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-11-09 15:18:18 +0800

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,7 +13,6 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const email = ref("");
 const password = ref("");
-const router = useRouter();
 
 
 const register = () => {
