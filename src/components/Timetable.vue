55c6548eae5a05b10f2e8340a2da7a7d1ed26286
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-11-10 16:20:56 +0800

    refactor: timetable component styling (#56)
    * refactor: timetable component styling
    
    * removed extra left margin on TimetableModule component
    
    Co-authored-by: Jerome Wong <jeromewongjh@gmail.com>

--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -1,50 +1,47 @@
 <template>
-
-    <div class="hidden lg:block">
-        <TimetableTimeRow></TimetableTimeRow>
-        <TimetableRow day="Mon"></TimetableRow>
-        <TimetableRow day="Tue"></TimetableRow>
-        <TimetableRow day="Wed"></TimetableRow>
-        <TimetableRow day="Thur"></TimetableRow>
-        <TimetableRow day="Fri"></TimetableRow>
-        <TimetableRow day="Sat"></TimetableRow>
-        <TimetableRow day="Sun"></TimetableRow>
+  <div class="min-w-[55rem] text-sm font-semibold">
+    <div class="flex justify-between ml-14">
+      <div v-for="hour in hours">{{ hour }}</div>
     </div>
-    <div class="lg:hidden">
-        <TimetableDayRowMobile></TimetableDayRowMobile>
-        <TimetableRowMobile time="0800"></TimetableRowMobile>
-        <TimetableRowMobile time="0900"></TimetableRowMobile>
-        <TimetableRowMobile time="1000"></TimetableRowMobile>
-        <TimetableRowMobile time="1100"></TimetableRowMobile>
-        <TimetableRowMobile time="1200"></TimetableRowMobile>
-        <TimetableRowMobile time="1300"></TimetableRowMobile>
-        <TimetableRowMobile time="1400"></TimetableRowMobile>
-        <TimetableRowMobile time="1500"></TimetableRowMobile>
-        <TimetableRowMobile time="1600"></TimetableRowMobile>
-        <TimetableRowMobile time="1700"></TimetableRowMobile>
-        <TimetableRowMobile time="1800"></TimetableRowMobile>
-        <TimetableRowMobile time="1900"></TimetableRowMobile>
-        <TimetableRowMobile time="2000"></TimetableRowMobile>
-        <TimetableRowMobile time="2100"></TimetableRowMobile>
-        <TimetableRowMobile time="2200"></TimetableRowMobile>
-
-        
+    <div
+      class="m-0 border rounded border-solid border-gray-400 dark:border-gray-600"
+    >
+      <ol>
+        <TimetableRow v-for="day in days" :key="day" :day="day" />
+      </ol>
     </div>
-    
-
-    
-    
+    <TimetableModule />
+  </div>
 </template>
 
 <script>
-    import TimetableRow from "@/components/TimetableRow.vue"
-    import TimetableTimeRow from "@/components/TimetableTimeRow.vue"
-    import TimetableDayRowMobile from "@/components/TimetableDayRowMobile.vue"
-    import TimetableRowMobile from "@/components/TimetableRowMobile.vue"
+import TimetableRow from "./TimetableRow.vue";
+import TimetableModule from "./TimetableModule.vue";
 
-
-    export default {
-        name: "Timetable",
-        components: { TimetableRow, TimetableTimeRow, TimetableDayRowMobile, TimetableRowMobile }
-    }
+export default {
+  name: "Timetable",
+  data() {
+    return {
+      days: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      hours: [
+        "0800",
+        "0900",
+        "1000",
+        "1100",
+        "1200",
+        "1300",
+        "1400",
+        "1500",
+        "1600",
+        "1700",
+        "1800",
+        "1900",
+        "2000",
+        "2100",
+        "2200",
+      ],
+    };
+  },
+  components: { TimetableRow },
+};
 </script>
