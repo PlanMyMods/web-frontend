8db14d6bc3a873ea43a325c213756a07e60e41f2
Jerome Wong <jeromewongjh@gmail.com>
2022-11-10 17:38:08 +0800

    partial implementation of prereq tree

--- src/components/ModulePage.vue
+++ src/components/ModulePage.vue
@@ -18,7 +18,7 @@
                 <div class="w-full lg:w-[50%] mx-auto lg:mx-0 mb-12 lg:mb-0  flex justify-center lg:justify-start">
                     <ModuleCardTerms :terms="moduleTerms" :courseLink="moduleData.link" />
                 </div>
-                <div class="  w-full lg:w-[50%] flex justify-center lg:justify-end align-middle">
+                <div class="  w-full lg:w-[50%] flex justify-center lg:justify-end items-center">
                     <div class="w-[300px]">
                         <DoughnutChart :assessmentName='assessmentArr' :weightage="weightageArr" />
                     </div>
@@ -28,12 +28,17 @@
 
             <!-- pre-req tree -->
             <div class="my-10">
-                <PrerequisiteTree :prerequisite='moduleData.prerequisite' :moduleName='moduleData.code' />
+                <PrerequisiteTree :prerequisites='prereqArr' :moduleName='moduleData.code' :isOr='isOr' />
             </div>
             <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
 
             <!-- timetable component -->
             <h1 class="text-3xl font-extrabold dark:text-gray-300 py-5 text-gray-600 ">Timetable</h1>
+            <div class="w-full overflow-x-auto mb-20">
+
+                <Timetable></Timetable>
+            </div>
+            <h1 class="text-3xl font-extrabold dark:text-gray-300 py-5 text-gray-600 ">{{ prereqArr }}</h1>
         </div>
     </div>
 </template>
@@ -45,15 +50,16 @@ import ModuleCardPrerequisite from '@/components/ModuleCardPrerequisite.vue';
 import ModuleCardTerms from '@/components/ModuleCardTerms.vue';
 import PrerequisiteTree from '@/components/PrerequisiteTree.vue'
 import DoughnutChart from './DoughnutChart.vue';
+import Timetable from './Timetable.vue'
 
 export default {
     async created() {
 
         this.moduleData = await getFullCoursebyCode('CS203')
-        this.moduleTerms = await getModuleTerms('CS203')
+        this.moduleTerms = await getModuleTerms('IS215')
         // this.moduleData = await getFullCoursebyCode(this.$route.params.id)
         // this.moduleTerms = await getModuleTerms(this.$route.params.id)
-        console.log('moduleData:', this.moduleData)
+        console.log('moduleData pre-req:', this.moduleData.prerequisites.string)
         console.log('moduleTerms:', this.moduleTerms[0].assessment)
         // console.log(this.$route.params.id)
     },
@@ -63,13 +69,29 @@ export default {
             return terms.length > 0;
         },
     },
+    computed: {
+        prereqArr() {
+            let arr = []
+            this.moduleData.prerequisites.ref.map((e) => {
+                arr.push(e.id)
+            })
+
+            return arr
+        }
+    },
+    isOr() {
+        if (this.moduleData.prerequisites.string.includes('or')) {
+            return true
+        }
+        return false
+    },
     data() {
         return {
             moduleData: [],
             moduleTerms: [],
-            assessmentArr: ['Class Participation', 'Exams', 'Project', 'Quizzes'],
 
             //TODO: this needs to be dynamically updated for each module
+            assessmentArr: ['Class Participation', 'Exams', 'Project', 'Quizzes'],
             weightageArr: [10, 35, 35, 20],
         }
     },
@@ -77,7 +99,8 @@ export default {
         ModuleCardPrerequisite,
         ModuleCardTerms,
         PrerequisiteTree,
-        DoughnutChart
+        DoughnutChart,
+        Timetable
     },
 
 }
--- src/components/PrerequisiteTree.vue
+++ src/components/PrerequisiteTree.vue
@@ -2,42 +2,58 @@
     <!-- not completed -->
     <h1 class="text-3xl font-extrabold dark:text-gray-300 py-5 text-gray-600 mb-10">Pre-requisite Tree
     </h1>
-    <div class="dark:bg-gray-800 flex justify-between text-center bg-gray-50 grid-cols-5 text-black dark:text-gray-300">
-        <div class="col-span-1 z-10 sm:p-3 p-1.5 rounded-lg border-blue-300 border-2 my-auto">
-            <div class="flex-col flex space-y-2">
-                <!-- prerequisite prop is supposed to be an array which we will loop through to display the module buttons -->
+    <div class="w-full overflow-x-auto">
+        <div
+            class="dark:bg-gray-800 flex justify-between text-center bg-gray-50 grid-cols-5 text-black dark:text-gray-300  ">
+            <div class="col-span-1 z-10 sm:p-3 p-1.5 rounded-lg border-blue-300 border-2 my-auto ">
+                <h5 class="text-sm font-bold dark:text-white pb-2 px-2">Prerequisites</h5>
+                <!-- displaying mods -->
+                <div v-if="{ isOr }" class="flex-row flex space-x-2">
+                    <button v-for="mod in prerequisites " type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 xs:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mx-auto">
+                        {{ mod }}
+                    </button>
+                </div>
+                <div v-else class="flex-col flex space-y-2">
+                    <button v-for="mod in prerequisites " type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 xs:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        {{ mod }}
+                    </button>
+                </div>
+            </div>
+            <hr class="my-auto min-w-[2rem] col-span-1 w-full h-1 bg-gray-300 rounded border-0 dark:bg-gray-700">
+            <!-- this is the module currently being viewed -->
+            <div class="col-span-1 z-10 sm:p-3 p-1.5 rounded-lg border-blue-300 border-2 my-auto">
                 <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 xs:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    {{ prerequisites }}
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    {{ moduleName }}
                 </button>
             </div>
-        </div>
-        <hr class="my-auto col-span-1 w-full h-1 bg-gray-300 rounded border-0 dark:bg-gray-700">
-        <!-- this is the module currently being viewed -->
-        <div class="col-span-1 z-10 sm:p-3 p-1.5 rounded-lg border-blue-300 border-2 my-auto">
-            <button type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                {{ moduleName }}
-            </button>
-        </div>
-        <hr class="my-auto col-span-1 w-full h-1 bg-gray-300 rounded border-0 dark:bg-gray-700">
+            <hr class="my-auto min-w-[2rem] col-span-1 w-full h-1 bg-gray-300 rounded border-0 dark:bg-gray-700">
 
-        <!-- dependency mods (requires advanced querying to retrieve information to display)-->
-        <!-- should also be an array of modules to loop through -->
-        <div class="col-span-1 z-10 sm:p-3 p-1.5  rounded-lg border-blue-300 border-2 my-auto">
-            <div class="flex-col flex space-y-2">
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Mods
-                </button>
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Mods
-                </button>
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Mods
-                </button>
+            <!-- dependency mods (requires advanced querying to retrieve information to display)-->
+            <!-- should also be an array of modules to loop through -->
+            <div class="col-span-1 z-10 sm:p-3 p-1.5  rounded-lg border-blue-300 border-2 my-auto">
+                <h5 class="text-sm font-bold dark:text-white pb-2 px-2">Dependencies</h5>
+
+                <div class="flex-col flex space-y-2">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Mods
+                    </button>
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Mods
+                    </button>
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Mods
+                    </button>
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Mods
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -49,17 +65,17 @@ export default {
     props: {
         moduleName: {
             type: String,
-            default: ' None'
+            default: 'None'
         },
 
         prerequisites: {
             type: Array,
-            default: 'None',
+            default: ['None'],
         },
-        corequisites: {
-            type: String,
-            default: null,
-        },
+        isOr: {
+            type: Boolean,
+            default: false
+        }
     },
     components: {
         Button
