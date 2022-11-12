67216f7c58516250e561634aa972ba751054aff6
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-11-12 21:53:05 +0800

    Update SelectModule.vue

--- src/components/SelectModule.vue
+++ src/components/SelectModule.vue
@@ -1,13 +1,14 @@
 <template>
 
   <!-- Step 1: use v-for to loop through the modules and include each one in the dropdown --> 
-  <div class = "dark:text-white">
-    <select id = "chooseMods" onchange = GetSelectedMod(this)>
-       <option v-for="mod of moduleCodeList">
-            {{mod}}
-       </option>
-    </select>
-  </div>
+  <Combobox v-model="selectedModule">
+    <ComboboxInput @change="query = $event.target.value" />
+    <ComboboxOptions>
+      <ComboboxOption v-for="mod in filteredModules" :key="mod" :value="mod">
+        {{ mod }}
+      </ComboboxOption>
+    </ComboboxOptions>
+  </Combobox>
 
   <!-- Step 2: Create a button "Add to Timetable" which adds the selected module to Timetable onclick
   Can use AddModule.vue component (to be added) here-->
@@ -16,42 +17,48 @@
 
 
 <script>
-    /* 
-    OVERVIEW: 
-    - Showcase a dropdown box displaying the modules 
-    - user can select, and press a button "Add to Calendar" if they want to display on calendar. 
-    */
-
-
+    import { ref } from 'vue'
+    import { Combobox, ComboboxInput, ComboboxOptions, ComboboxOption } from '@headlessui/vue'
     // import returnModuleArray to get the Codes for each module. These will be included in the dropdown
     import { returnModuleArray } from "@/utils/firebase";
-    import Button from "@/components/Button.vue";
 
+
+   
 
     // export to allow AddModule to import this later
     export default {
     name: "SelectModule",
 
+
+    
     async created() {
       this.moduleCodeList = await returnModuleArray();
-  },
+    },
+
+
     data (){
       return {
-        moduleCodeList: []
+        moduleCodeList: [], 
+        selectedModule: ref(moduleCodeList[0]),
+        query: ref(""),
+        
       }
     },
 
-    methods: {
-        GetSelectedMod(chooseMods){
-          
-        }
-    },
+    components: { Combobox, ComboboxInput, ComboboxOptions, ComboboxOption },
 
-    components: { Button }
-};
+    computed: {
+        filterModules() {
+          query.value === ''
+          ? moduleCodeList
+          : moduleCodeList.filter((mod) => {
+        return mod.toLowerCase().includes(query.value.toLowerCase())
+        }
+          )
+    }
+  }, 
 
     
-
-
+};
 
 </script>
