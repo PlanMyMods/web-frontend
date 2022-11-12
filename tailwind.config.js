aec7684d57ecfc0751f32e31aae85a946a4be69f
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-11-13 01:35:04 +0800

    chore: add icons to sidebar (#61)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     screens: {
@@ -10,6 +10,25 @@ module.exports = {
       lg: "1024px",
       xl: "1280px",
       "2xl": "1280px",
+    },
+    animation: {
+      shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+    },
+    keyframes: {
+      shake: {
+        "10%, 90%": {
+          transform: "translate3d(-1px, 0, 0)",
+        },
+        "20%, 80%": {
+          transform: "translate3d(2px, 0, 0)",
+        },
+        "30%, 50%, 70%": {
+          transform: "translate3d(-4px, 0, 0)",
+        },
+        "40%, 60%": {
+          transform: "translate3d(4px, 0, 0)",
+        },
+      },
     },
   },
   plugins: [],
