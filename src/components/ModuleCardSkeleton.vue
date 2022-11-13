90c1b6d077283460430f3f557d58658650ad2817
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-11-13 23:16:46 +0800

    bugfix: cards skeleton ui not wrapping (#71)

--- src/components/ModuleCardSkeleton.vue
+++ src/components/ModuleCardSkeleton.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="bg-gray-50 dark:bg-gray-800 px-3 py-6 sm:px-6 md:p-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col xl:flex-row justify-between max-w-screen-2xl lg:flex-row h-[16.2rem] animate-pulse"
+    class="bg-gray-50 dark:bg-gray-800 px-3 py-6 sm:px-6 md:p-8 lg:px-16 lg:py-9 rounded-2xl flex flex-col sm:flex-row justify-between max-w-screen-2xl w-full lg:flex-row h-[16.2rem] animate-pulse"
   >
-    <div class="grid content-center space-y-5 w-[55%]">
+    <div class="grid content-center space-y-5 w-[100%] sm:w-[55%]">
       <div class="grid grid-cols-6 gap-4">
         <div class="h-2 bg-gray-300 dark:bg-slate-700 rounded col-span-2"></div>
         <div class="h-2 bg-gray-300 dark:bg-slate-700 rounded col-span-3"></div>
@@ -26,7 +26,7 @@
         <div class="h-2 bg-gray-300 dark:bg-slate-700 rounded col-span-4"></div>
       </div>
     </div>
-    <div class="grid content-center space-y-5 w-[40%]">
+    <div class="hidden sm:grid content-center space-y-5 sm:w-[40%]">
       <div class="grid grid-cols-6 gap-4">
         <div class="h-2 bg-gray-300 dark:bg-slate-700 rounded col-span-2"></div>
         <div class="h-2 bg-gray-300 dark:bg-slate-700 rounded col-span-3"></div>
