a645c144c2d906db6bce87d7b627844800e727ec
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-11-13 21:36:10 +0800

    Update ChangeEmail.vue

--- src/components/ChangeEmail.vue
+++ src/components/ChangeEmail.vue
@@ -18,7 +18,7 @@
         </div>
 
         <div>
-            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Submit</button>
+            <button type="button" @click = "resetEmail()" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Submit</button>
         </div>
     </form>
 
