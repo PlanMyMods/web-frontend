cfe017f5c1c2c7dbf7f7670d6b7016ad109b3216
Gabriel Wong <gabrielwong.2021@scis.smu.edu.sg>
2022-11-13 21:34:17 +0800

    Functions for reset email and reset password

---
+++ src/components/ChangeEmail.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class = "container mx-auto bg-inherit bg-transparent rounded-2xl pt-10 pb-10 pl-5 pr-5">
+        <h1 class = "text-2xl font-medium">Change Email</h1>
+    <form>
+        <label for="input-group-1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Enter New Email</label>
+        <div class="relative mb-6">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+        </div>
+        <input type="text" id="input-group-1" v-model="newEmail" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="johndoe@smu.edu.sg">
+        </div>
+        <label for="input-group-1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm New Email</label>
+        <div class="relative mb-6">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+        </div>
+        <input type="text" id="input-group-1" v-model="newEmailConfirmed" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="johndoe@smu.edu.sg">
+        </div>
+
+        <div>
+            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Submit</button>
+        </div>
+    </form>
+
+    </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+import { auth } from "@/utils/firebase";
+import Settings from "@/views/SettingsView.vue";
+import { updateEmail } from "firebase/auth"
+
+    export default {
+        name: "ChangeEmail",
+
+        data(){
+            return{
+                newEmail: "",
+                newEmailConfirmed: "",
+            }
+        }, 
+
+        methods: {
+
+            resetEmail: function() {
+                var emailNew = this.newEmail
+
+                var user = auth.currentUser;
+                console.log ("  User:  "+ user );
+
+                updateEmail(emailNew).then(function() {
+                // Update successful.
+                alert("Reset Email successful")
+                }).catch(function(error) {
+                // An error happened.
+                });
+
+},
+
+}
+    }
+
+
+</script>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -4,10 +4,24 @@
       <div class="col-md-8">
         <div class="card">
           <div v-if="user.loggedIn">
-            <div class="card-header">Welcome, {{ user.data.displayName }}</div>
+            <div class="card-header">Welcome, 
+              <span class = "font-small leading-tight text-2xl mt-0 mb-2 text-black-600">{{ user.data.displayName }}
+            </span>
+            </div>
             <div class="card-body">
-              <div class="alert alert-success" role="alert">
-                You are logged in!
+              <div class="alert alert-success mt-6" role="alert">
+                <h2 class="font-small leading-tight text-3xl mt-0 mb-2 text-black-600">
+                  You are logged in!
+                </h2>
+
+              <div class = "mt-6">
+                <ChangeEmail/>
+              </div>
+
+              <div class = "mt-6">
+                <ChangePassword/>
+              </div>
+                
                 <div class="my-4">
                   <button @click.prevent="signOut" class="btn btn-primary">
                     Log Out
@@ -30,34 +44,30 @@ import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { computed } from "vue";
 import { auth } from "@/utils/firebase";
+import ChangeEmail from "@/components/ChangeEmail.vue";
+import ChangePassword from "@/components/ChangePassword.vue";
 
 export default {
-  name: "Settings",
-
-  setup() {
-    const store = useStore();
-    const router = useRouter();
-
-    auth.onAuthStateChanged((user) => {
-      store.dispatch("fetchUser", user);
-    });
-
-    const user = computed(() => {
-      return store.getters.user;
-    });
-
-    const signOut = async () => {
-      await store.dispatch("logOut");
-      router.push("/");
-    };
-
-    return { user, signOut };
-  },
-
-  methods: {
-    updateUserInfo(){
-      
-    }
-  }
+    name: "Settings",
+    setup() {
+        const store = useStore();
+        const router = useRouter();
+        auth.onAuthStateChanged((user) => {
+            store.dispatch("fetchUser", user);
+        });
+        const user = computed(() => {
+            return store.getters.user;
+        });
+        const signOut = async () => {
+            await store.dispatch("logOut");
+            router.push("/");
+        };
+        return { user, signOut };
+    },
+    methods: {
+        updateUserInfo() {
+        }
+    },
+    components: { ChangeEmail, ChangePassword }
 };
 </script>
