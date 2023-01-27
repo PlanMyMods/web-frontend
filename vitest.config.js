a98ce1785d88af3aa6d4d027a5a54297ad3360c8
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-01-27 13:37:34 +0800

    chore: update github actions to include tests

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,12 @@ import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [Vue()],
+  exclude: [
+    "**/node_modules/**",
+    "**/dist/**",
+    "./playground/**/*.*",
+    "./playground-temp/**/*.*",
+  ],
   test: {
     globals: true,
     environment: "jsdom",
